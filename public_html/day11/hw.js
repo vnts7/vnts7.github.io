bfa3ff8099ea29cf6159d70ec5278af928d01021
Tai Nguyen <vannguyents@gmail.com>
2019-05-13 14:58:25 -0500

    Day11 fix validator

--- public_html/day11/hw.js
+++ public_html/day11/hw.js
@@ -1,41 +1,76 @@
-let speed = 250;
-let timer = null;
-let oldText = null;
-let frames = [];
-let frameIdx = 0;
-function animationChange(e) {
-    document.getElementById('text').value = ANIMATIONS[e.value];
-}
-function sizeChange(e) {
-    document.getElementById('text').style.fontSize = e.value;
-}
-function speedChange(e) {
-    speed = e.checked ? 50 : 250;
-    if(timer)clearInterval(timer);
-    startAnimation();
-}
-function startClick(e) {
-    e.disabled = true;
-    document.getElementById('btnStop').disabled = false;
-    let text = document.getElementById('text');
-    oldText = text.value;
-    frames = oldText.split('=====\n');
-    frameIdx = 0;
-    startAnimation();
-}
-function startAnimation(){
-    timer = setInterval(() => {
-        document.getElementById('text').value = frames[frameIdx];
-        frameIdx++;
-        if (frameIdx == frames.length) frameIdx = 0;
-    }, speed);
-}
-function stopClick(e) {
-    e.disabled = true;
-    document.getElementById('btnStart').disabled = false;
-    if (timer) {
-        clearInterval(timer);
-        document.getElementById('text').value = oldText;
-        timer = null;
+(function () {
+    "use strict";
+    const hiSpeed = 50, loSpeed = 250;
+    let speed = loSpeed;
+    let timer = null;
+    let oldText = null;
+    let frms = null;
+    let frameIdx = 0;
+    /**
+     * getElementById
+     * @param {*} id element id
+     *@returns {HTMLElement} HTML element
+     */
+    function $(id) {
+        return document.getElementById(id);
     }
-}
+    /**
+     * Handle animation change
+     */
+    function animationChange() {
+        $('text').value = window.ANIMATIONS[this.value];
+    }
+    /**
+     * Handle size change
+     */
+    function sizeChange() {
+        $('text').style.fontSize = this.value;
+    }
+    /**
+     * Handle speedChange
+     */
+    function speedChange() {
+        speed = this.checked ? hiSpeed : loSpeed;
+        if (timer) { clearInterval(timer); }
+        startAnimation();
+    }
+    /**
+     * Handle startClick
+     */
+    function startClick() {
+        let text = $('text');
+        oldText = text.value;
+        frms = oldText.split('=====\n');
+        frameIdx = 0;
+        startAnimation();
+        this.disabled = true;
+        $('btnStop').disabled = false;
+    }
+    /**
+     * start the Animation
+     */
+    function startAnimation() {
+        timer = setInterval(() => {
+            $('text').value = frms[frameIdx];
+            frameIdx++;
+            if (frameIdx === frms.length) { frameIdx = 0; }
+        }, speed);
+    }
+    /**
+     * Handle stopClick
+     */
+    function stopClick() {
+        this.disabled = true;
+        $('btnStart').disabled = false;
+        if (timer) {
+            clearInterval(timer);
+            $('text').value = oldText;
+            timer = null;
+        }
+    }
+    $('size').onchange = sizeChange;
+    $('speed').onchange = speedChange;
+    $('animation').onchange = animationChange;
+    $('btnStart').onclick = startClick;
+    $('btnStop').onclick = stopClick;
+})();
